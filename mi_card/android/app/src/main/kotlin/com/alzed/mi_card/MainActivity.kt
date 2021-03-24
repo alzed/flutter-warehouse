d05eaa028a448ee498ec7f682c893bdf91501b8d
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-03-24 13:38:37 +0530

    Add mi_card

---
+++ mi_card/android/app/src/main/kotlin/com/alzed/mi_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alzed.mi_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mi_card/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/avatar.jpg'),
+                radius: 100.0,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text('Awesome Triangles',
+                  style: GoogleFonts.merienda(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  )),
+              SizedBox(
+                height: 40.0,
+                width: 100.0,
+                child: Divider(
+                  thickness: 1.0,
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call,
+                    color: Colors.blueGrey[900],
+                  ),
+                  title: Text(
+                    '+91 12345 67890',
+                    style: GoogleFonts.merienda(
+                      color: Colors.blueGrey[900],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.blueGrey[900],
+                  ),
+                  title: Text(
+                    'alzed@example.com',
+                    style: GoogleFonts.merienda(
+                      color: Colors.blueGrey[900],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
