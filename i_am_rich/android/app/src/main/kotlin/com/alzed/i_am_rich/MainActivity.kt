8a60e102d8448ea860f26435f35f8d021816fd99
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-03-24 13:38:20 +0530

    Add IAmRich

---
+++ i_am_rich/android/app/src/main/kotlin/com/alzed/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alzed.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(IAmRich());
+}
+
+class IAmRich extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
