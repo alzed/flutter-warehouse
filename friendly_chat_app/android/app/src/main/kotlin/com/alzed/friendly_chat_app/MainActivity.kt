d8f90726bf9cd5c4e0c9463730ec4857bcc37332
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-03-26 20:39:13 +0530

    friendly_chat_app - Initial commit

---
+++ friendly_chat_app/android/app/src/main/kotlin/com/alzed/friendly_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alzed.friendly_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ friendly_chat_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'FriendlyChat',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FriendlyChat'),
+        ),
+      ),
+    ),
+  );
+}
