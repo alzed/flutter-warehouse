f4d1fa2daaf4ad43ec2c5ab4435d4e9c7c6f48d0
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-03-27 22:33:13 +0530

    Create stateless widget

--- friendly_chat_app/lib/main.dart
+++ friendly_chat_app/lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(FriendlyChatApp());
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'FriendlyChat',
       home: Scaffold(
         appBar: AppBar(
           title: Text('FriendlyChat'),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
