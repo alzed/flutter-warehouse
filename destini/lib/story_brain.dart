9c37836ed69a5b35d584fe62fb27b85fe7c6d18d
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-04-07 15:55:23 +0530

    Add story flow

--- destini/lib/story_brain.dart
+++ destini/lib/story_brain.dart
@@ -47,15 +47,31 @@ class StoryBrain {
     return _storyData[0].choice2;
   }
 
-  void nextStory(int choiceNumber) {}
+  void nextStory(int choiceNumber) {
+    if (storyNumber == 0) {
+      if (choiceNumber == 1) {
+        storyNumber = 2;
+      } else {
+        storyNumber = 1;
+      }
+    } else if (storyNumber == 1) {
+      if (choiceNumber == 1) {
+        storyNumber = 2;
+      } else {
+        storyNumber = 3;
+      }
+    } else if (storyNumber == 2) {
+      if (choiceNumber == 1) {
+        storyNumber = 5;
+      } else {
+        storyNumber = 4;
+      }
+    }
+  }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
