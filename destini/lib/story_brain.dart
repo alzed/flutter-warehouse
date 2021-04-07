51d60183505f16c32c225d03909d8fa6c88a09ef
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-04-07 16:16:24 +0530

    Add restart at the end of story

--- destini/lib/story_brain.dart
+++ destini/lib/story_brain.dart
@@ -59,21 +59,25 @@ class StoryBrain {
         storyNumber = 2;
       } else {
         storyNumber = 3;
+        restart();
       }
     } else if (storyNumber == 2) {
       if (choiceNumber == 1) {
         storyNumber = 5;
+        restart();
       } else {
         storyNumber = 4;
+        restart();
       }
     }
   }
 
+  void restart() {
+    storyNumber = 0;
+  }
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
 }
