c127033db886c6f7e8bd313c7812a8198dc1df03
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-04-07 14:19:44 +0530

    Display choice1 and choice2

--- destini/lib/main.dart
+++ destini/lib/main.dart
@@ -61,8 +61,7 @@ class _StoryPageState extends State<StoryPage> {
                         MaterialStateProperty.all<Color>(Colors.red),
                   ),
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                       color: Colors.white,
@@ -87,8 +86,7 @@ class _StoryPageState extends State<StoryPage> {
                         MaterialStateProperty.all<Color>(Colors.blue),
                   ),
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                       color: Colors.white,
--- destini/lib/story_brain.dart
+++ destini/lib/story_brain.dart
@@ -36,13 +36,17 @@ class StoryBrain {
   String getStory() {
     return _storyData[0].storyTitle;
   }
+
+  String getChoice1() {
+    return _storyData[0].choice1;
+  }
+
+  String getChoice2() {
+    return _storyData[0].choice2;
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
-
-//TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
