1d205d83c78f1742d9e080a87b1dac52befd9dfa
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-06-02 01:16:56 +0530

    Extract dropdown and textfield widgets

---
+++ bitcoin_ticker/lib/coin_dropdown_field.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CoinDropdownField extends StatelessWidget {
+  const CoinDropdownField({
+    Key key,
+    @required this.coin,
+    @required this.coinColor,
+    @required this.coinDropdownColor,
+    @required this.coinList,
+    @required this.coinUpdateFunction,
+  }) : super(key: key);
+
+  final String coin;
+  final Color coinColor;
+  final Color coinDropdownColor;
+  final List<String> coinList;
+  final Function coinUpdateFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton(
+        value: coin,
+        icon: Icon(
+          Icons.arrow_drop_down,
+          color: coinColor,
+          size: 40.0,
+        ),
+        dropdownColor: coinDropdownColor,
+        onChanged: (String coin) {
+          coinUpdateFunction(coin);
+        },
+        items: coinList.map<DropdownMenuItem<String>>((String coin) {
+          return DropdownMenuItem<String>(
+            value: coin,
+            child: Text(
+              coin,
+              style: TextStyle(
+                color: coinColor,
+                fontSize: 30.0,
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
