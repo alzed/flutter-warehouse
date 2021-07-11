4bc6271d100e88e287cab8b3b88c153801368f75
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-07-11 23:53:35 +0530

    Rename signin_screen.dart

---
+++ flashchat/lib/signin_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'auth_button.dart';
+
+class SigninScreen extends StatefulWidget {
+  const SigninScreen({Key key}) : super(key: key);
+
+  static String id = 'signin_screen';
+
+  @override
+  _SigninScreenState createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Hero(
+              tag: 'logo',
+              child: Icon(
+                Icons.flash_on,
+                color: Colors.yellow.shade600,
+                size: 120.0,
+              ),
+            ),
+            Form(
+              child: Column(
+                children: [
+                  TextField(),
+                  TextField(),
+                ],
+              ),
+            ),
+            AuthButton(
+              content: 'Sign In',
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
