17b4f5ac34fc73a99a460108dd2eb7bf57693ea1
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-07-11 23:52:52 +0530

    Create chat_screen.dart

---
+++ flashchat/lib/chat_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key key}) : super(key: key);
+
+  static String id = 'chat_screen';
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
