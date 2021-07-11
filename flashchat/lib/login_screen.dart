b71e6075885cdaec010b505f6fccd5c1dcbc5565
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-07-11 23:52:58 +0530

    Create login_screen.dart

---
+++ flashchat/lib/login_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'auth_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key key}) : super(key: key);
+
+  static String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Hero(
+              tag: 'logo',
+              child: Icon(
+                Icons.flash_on,
+                color: Colors.yellow.shade600,
+                size: 120.0,
+              ),
+            ),
+            Form(
+              child: Column(
+                children: [
+                  TextField(),
+                  TextField(),
+                ],
+              ),
+            ),
+            AuthButton(
+              content: 'Log In',
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
