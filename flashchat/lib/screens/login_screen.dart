675546ae19845e89ded6f53379b112a0ea605deb
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-07-18 23:14:05 +0530

    Remove AuthForm and add form to login and signup screen

--- flashchat/lib/screens/login_screen.dart
+++ flashchat/lib/screens/login_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flashchat/screens/chat_screen.dart';
 
-import '../components/auth_form.dart';
+import 'package:flashchat/components/auth_button.dart';
+import 'package:flashchat/components/email_form_field.dart';
+import 'package:flashchat/components/password_form_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key key}) : super(key: key);
@@ -12,6 +16,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _auth = FirebaseAuth.instance;
+
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,9 +38,48 @@ class _LoginScreenState extends State<LoginScreen> {
                 size: 120.0,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: AuthForm(type: 'login'),
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    EmailFormField(
+                      onChanged: (value) {
+                        email = value;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    PasswordFormField(
+                      onChanged: (value) {
+                        password = value;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    AuthButton(
+                      content: 'Log in',
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          try {
+                            final user = await _auth.signInWithEmailAndPassword(
+                                email: email, password: password);
+                            if (user != null) {
+                              Navigator.pushNamed(context, ChatScreen.id);
+                            }
+                          } catch (e) {
+                            print(e);
+                          }
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
