6018c75a1e1897341d97f1226b3b363559219e66
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-09-18 00:24:56 +0530

    Add tasks to task screen

--- todoey/lib/screens/task_screen.dart
+++ todoey/lib/screens/task_screen.dart
@@ -36,6 +36,59 @@ class TaskScreen extends StatelessWidget {
                 ],
               ),
             ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25.0),
+                    topRight: Radius.circular(25.0),
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text('You have 0 tasks'),
+                    ListView(
+                      shrinkWrap: true,
+                      children: [
+                        ListTile(
+                          title: Text('Task1'),
+                          trailing: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.check_box_outline_blank,
+                              color: Color(0xFF004080),
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          title: Text('Task2'),
+                          trailing: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.check_box_outline_blank,
+                              color: Color(0xFF004080),
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          title: Text('Task3'),
+                          trailing: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.check_box_outline_blank,
+                              color: Color(0xFF004080),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
