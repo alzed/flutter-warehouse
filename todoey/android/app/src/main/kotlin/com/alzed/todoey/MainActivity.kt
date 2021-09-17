4a169623232e4449080511e1e81ade702cf4f2e1
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-09-18 00:14:44 +0530

    Todoey - Initial commit

---
+++ todoey/android/app/src/main/kotlin/com/alzed/todoey/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alzed.todoey
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ todoey/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoeyApp());
+}
+
+class TodoeyApp extends StatelessWidget {
+  const TodoeyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
